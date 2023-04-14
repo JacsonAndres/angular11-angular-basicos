1c00e23137f03299caaf7d24e8d599923bd6106b
JacsonAndres <jacsonburgos1@gmail.com>
2023-04-14 10:58:12 -0500

    primer commit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ContadorComponent } from './contador/Contador.component';
+
+@NgModule({
+  declarations: [ContadorComponent],
+  exports: [ContadorComponent],
+})
+export class ContadorModule {}
